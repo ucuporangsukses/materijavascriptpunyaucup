613d12784e41f2333682499d46d9f71ee69719a8
ysuf123 <ramadhaniy660@gmail.com>
2025-09-24 21:37:56 +0700

    Delete TUGAS_13 directory

--- TUGAS_13/catatan.js
+++
@@ -1,8 +0,0 @@
-import sayHay, {salamDenganUmur} from "./export.js";
-
-
-// sayHay("Bambang","Ucup","Ujang","Udin","Jono","Mulyono","Zidan",);
-
-salamDenganUmur("Udin","90")
-
-// console.log(subject("BeLajAr Js DasAR","BeLajAr Js DasAR","BeLajAr Js DasAR"));
